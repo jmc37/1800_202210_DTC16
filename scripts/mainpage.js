3744fe3e098e6cb67a70314c18a2966d17aa288d
Jack Berena <jberenaa@gmail.com>
2022-03-03 21:58:28 -0800

    worked on guide examples

--- scripts/mainpage.js
+++ scripts/mainpage.js
@@ -22,3 +22,32 @@ function writeGuides() {
         Bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget maximus magna, vel dictum purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus"
     });
 };
+
+function displayCards(collection) {
+    let cardTemplate = document.getElementById("guideCardTemplate");
+
+    db.collection(collection).get()
+        .then(snap => {
+            var i = 1;
+            snap.forEach(doc => { //iterate thru each doc
+                var title = doc.data().name;   // get value of the "name" key
+                var details = doc.data().details;   // get value of the "details" key
+                let newcard = cardTemplate.content.cloneNode(true);
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-image').src = "./images/" + collection + ".jpg"; //hikes.jpg
+
+                //give unique ids to all elements for future use
+                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+                i++;
+            })
+        })
+}
+displayCards("Guides");
--- scripts/script.js
+++ scripts/script.js
@@ -137,3 +137,6 @@ function writeWebcamData() {
             })
     })
 }
+
+
+
