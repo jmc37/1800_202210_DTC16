0931c949b4f79e23758dffc3a2b9308ad60bd07a
SUNG HYUN HONG <seonghyun91@gmail.com>
2022-03-24 20:44:32 -0700

    implement backend of guidepage

--- scripts/guide.js
+++ scripts/guide.js
@@ -1,14 +1,27 @@
 function displayguide() {
-    db.collection("Guides").doc("5fIBWAYO8ZfxcRuJhKRa").get()
-        .then(guideDoc => {
-            //get the data fields of the user
-            var guidename = guideDoc.data().name;
-            var guidebio = guideDoc.data().Bio;
-            var guidecity = guideDoc.data().city;
-            var guidelanguage = guideDoc.data().languages;
-            var guideid = guideDoc.data().id;
-            var tourtitle = guideDoc.data().title;
-            document.getElementById('tour_title').innerHTML = tourtitle;
+
+    //hardcoded for testing. replace it after indiv tourpage is done.
+    // db.collection("Guides").doc("5fIBWAYO8ZfxcRuJhKRa").get()
+
+    // db.collection("Guides").where("name", "==", __guideName or guideID _from_main_or_tourpage)
+    db.collection("Guides").where("city", "==", "Vancouver")
+    .get()
+        .then(doc => {
+
+            guideDocs = doc.docs;
+            thisGuide = guideDocs[0].data();
+            //get the data fields of only one guide who is result of query
+            console.log(thisGuide);
+            var guidename = thisGuide.name;
+            var guidebio = thisGuide.Bio;
+            var guidecity = thisGuide.city;
+            var guidelanguage = thisGuide.languages;
+            var guideid = thisGuide.id;
+            var guide_tourtitle = thisGuide.tourtitle;
+
+
+            
+            document.getElementById('tour_title').innerHTML = guide_tourtitle;
             document.getElementById('name').innerHTML = guidename;
             document.getElementById('city').innerHTML = guidecity;
             document.getElementById('bio').innerHTML = guidebio;
