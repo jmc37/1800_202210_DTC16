ecbcc5e1f94ecfdcdbc8907372aa192c40316693
SUNG HYUN HONG <seonghyun91@gmail.com>
2022-03-30 18:12:56 -0700

    fixed createtour

--- scripts/createtour.js
+++ scripts/createtour.js
@@ -1,27 +1,27 @@
-// var tourTitle = null
-// var description = null
-// var image = null
+var tourTitle = null
+var description = null
+var image = null
 
 
 function populateInfo() {
     console.log('works')
-            //go to the correct user document by referencing to the user uid
-            tourT = $("#inputTour").val()
-            descript = $("#description").val()
-            tourImg = $("#tourImage").val()
-            //define a variable for the collection you want to create in Firestore to populate data
-            var tourRef = db.collection("tours");
+    //go to the correct user document by referencing to the user uid
+    tourT = $("#inputTour").val()
+    descript = $("#description").val()
+    tourImg = $("#tourImage").val()
+    //define a variable for the collection you want to create in Firestore to populate data
 
-            tourRef.add({
-                tourTitle: tourT, //Title of tour
-                description: descript, //Tour description
-                tourImage: tourImg, //Url to picture of tour
-                city: "Vancouver" //City added
-            });
+    var tourRef = db.collection("tours")
+    tourRef.add({
+        tourTitle: tourT, //Title of tour
+        description: descript, //Tour description
+        tourImage: tourImg, //Url to picture of tour
+        city: "Vancouver" //City added
+    });
 }
 
-function setup(){
-$('#submit').click(populateInfo)
+function setup() {
+    $('#submit').click(populateInfo);
 }
 
-jQuery(document).ready(setup)
+$(document).ready(setup);
