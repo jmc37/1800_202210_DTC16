25bb9271fa91ea6d31b84c21ebdbf01cd6c6f734
SUNG HYUN HONG <seonghyun91@gmail.com>
2022-04-05 19:59:15 -0700

    Correct profile button(no more infinite login)

--- scripts/search_result.js
+++ scripts/search_result.js
@@ -5,7 +5,7 @@ firebase.auth().onAuthStateChanged(user => {
         console.log(currentUser);
 
         // the following functions are always called when someone is logged in
-        populateCardsDynamically();
+
     } else {
         // No user is signed in.
         console.log("No user is signed in");
@@ -77,6 +77,8 @@ function populateCardsDynamically() {
 
         })
 }
+
+populateCardsDynamically();
 //-----------------------------------------------------------------------------
 // This function is called whenever the user clicks on the "bookmark" icon.
 // It adds the hike to the "bookmarks" array
