1d4a35126a41f7c5e5ea8e98a9ac974635b77c84
Joseph Chan <josephchan36@outlook.com>
2022-04-07 00:16:36 -0700

    Updated createtour button

--- scripts/search_result.js
+++ scripts/search_result.js
@@ -108,27 +108,3 @@ function setup() {
 
 $(document).ready(setup);
 
-// function writeGuides() {
-//     //define a variable for the collection you want to create in Firestore to populate data
-//     var GuideRef = db.collection("Tour");
-
-//     GuideRef.add({
-//         name: "Kyle G.",
-//         city: "Burnaby,BC",
-//         languages: "Tagalog,English",
-//         Bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget maximus magna, vel dictum purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus"
-
-//     });
-//     GuideRef.add({
-//         name: "Sanghoon L.",
-//         city: "Vancouver,BC",
-//         languages: "Korean,English",
-//         Bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget maximus magna, vel dictum purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus"
-//     });
-//     GuideRef.add({
-//         name: "Luca H.",
-//         city: "Whistler,BC",
-//         languages: "Vietnamese,English",
-//         Bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eget maximus magna, vel dictum purus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus"
-//     });
-// };
