b93153842efbc7d2c42a8eb87c0fffd6a2e3e15e
Jack Berena <jberenaa@gmail.com>
2022-04-07 14:50:20 -0700

    worked on cancel feature and added logo

--- scripts/booked.js
+++ scripts/booked.js
@@ -1,4 +1,19 @@
-var tourID=null
+var currentUser;
+firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        currentUser = db.collection("users").doc(user.uid); //global
+        currentUser.get().then(userDoc => {
+        //get the user name
+        var user_Name = userDoc.data().name;
+        console.log(user_Name);
+        })
+        // the following functions are always called when someone is logged in
+
+    } else {
+        // No user is signed in.
+        console.log("No user is signed in");
+    }
+});
 
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
@@ -36,6 +51,7 @@ function getBookmarks(user) {
                         newCard.querySelector('.get_detail').href = "tour.html?title=" + title + "&id=" + tourID;
                         newCard.querySelector('img').src = doc.tourImage;
                         hikeCardGroup.appendChild(newCard);
+                        document.getElementById('cancel_btn').onclick = () => cancelTour();
                     } else {
                         console.log("Query has more than one data")
                     }
@@ -51,7 +67,31 @@ function setTourData(tourID){
     localStorage.setItem('tourID', tourID);
 }
 
+let BookID = localStorage.getItem("tourID")
 
+function cancelTour() {
+    currentUser.update({
+            booked: firebase.firestore.FieldValue.arrayRemove()
+    })
+    .then(function () {
+        console.log("Tour has been cancelled");
+        // var iconID = 'save-' + hikeID;
+        //console.log(iconID);
+        document.getElementById('cancel_btn').innerText = 'Cancelled';
+    });
+}
+// function saveBookmark(tourID) {
+//     currentUser.set({
+//             bookmarks: firebase.firestore.FieldValue.arrayUnion(tourID)
+//         }, {
+//             merge: true
+//         })
+//         .then(function () {
+//             console.log("bookmark has been saved for: " + currentUser);
+//             var iconID = 'save-' + tourID;
+//             //console.log(iconID);
+//             document.getElementById(iconID).innerText = 'bookmark';
+//         });
 
 // function setup() {
 //     $('#review_btn').click(setTourID);
